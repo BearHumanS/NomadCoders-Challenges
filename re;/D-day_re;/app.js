67b4fa6b7c8c17b891ff77bb8f21130fc6eade01
BearHumanS <bye0651@naver.com>
2022-12-27 23:40:40 +0900

    first commit

--- re;/D-day_re;/app.js
+++ re;/D-day_re;/app.js
@@ -3,8 +3,7 @@ const main = document.querySelector("#main");
 function mainHandle(event) {
     const Hidden = "hidden";
     event.preventDefault();
-    main.classList.add(Hidden);
-    clock.classList.remove(Hidden);
+   clock.classList.remove(Hidden);
 }
 
 main.addEventListener("submit", mainHandle);
