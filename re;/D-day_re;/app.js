daa9b3fcf1de50e220ea110b2fd2a144323f3685
BearHumanS <bye0651@naver.com>
2023-01-03 17:06:17 +0900

    first commit

--- re;/D-day_re;/app.js
+++ re;/D-day_re;/app.js
@@ -1,4 +1,5 @@
 const main = document.querySelector("#main");
+const title = document.querySelector(".title")
 
 function mainHandle(event) {
     const Hidden = "hidden";
@@ -6,4 +7,14 @@ function mainHandle(event) {
    clock.classList.remove(Hidden);
 }
 
+function mouseEnter() {
+    title.innerText = "By. 아라리";
+}
+
+function mouseLeave() {
+    title.innerText = "디데이 계산기";
+}
+
 main.addEventListener("submit", mainHandle);
+title.addEventListener("mouseenter", mouseEnter);
+title.addEventListener("mouseleave", mouseLeave);
